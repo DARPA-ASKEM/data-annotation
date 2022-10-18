5194dff95db717b9f28806286569982c1b2fdd8a
Powell <powellfendley@gmail.com>
2022-10-18 11:35:30 -0400

    Final state for table editing demo

--- phantom/ui/client/datasets/Sheet.js
+++ phantom/ui/client/datasets/Sheet.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable indent */
 import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
@@ -12,49 +14,49 @@ import 'handsontable/dist/handsontable.full.min.css';
 import { Button } from '@material-ui/core';
 
 import LoadingOverlay from '../components/LoadingOverlay';
+import tableImg from '../assets/xDD-Demo-image.png';
 
 registerAllModules();
 
-
 const Sheet = withStyles(({ spacing }) => ({
     root: {
-      display: 'flex',
-      padding: [[0, spacing(4), spacing(2), spacing(4)]],
-      flexDirection: 'column',
-      flex: 1,
-      height: '100%'
+        display: 'flex',
+        padding: [[0, spacing(4), spacing(2), spacing(4)]],
+        flexDirection: 'column',
+        flex: 1,
+        height: '100%'
     },
     header: {
     },
     loadingWrapper: {
-      display: 'flex',
-      flex: '1 1 10rem',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column',
-      '@media (min-height: 700px)': {
-        marginTop: '-15%'
-      }
+        display: 'flex',
+        flex: '1 1 10rem',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        '@media (min-height: 700px)': {
+            marginTop: '-15%'
+        }
     },
     failedIcon: {
-      fontSize: '9rem',
-      opacity: 0.5
+        fontSize: '9rem',
+        opacity: 0.5
     },
     failedChipMessage: {
-      borderBottom: '1px solid gray',
-      marginBottom: '0.75rem'
+        borderBottom: '1px solid gray',
+        marginBottom: '0.75rem'
     },
     preformattedOutput: {
-      width: '75%',
-      padding: '1.25rem',
-      background: '#DDDDDD44',
-      overflow: 'auto',
-      maxHeight: '30%'
+        width: '75%',
+        padding: '1.25rem',
+        background: '#DDDDDD44',
+        overflow: 'auto',
+        maxHeight: '30%'
     }
-  }))(({
+}))(({
     classes, datasetInfo, setDatasetInfo, stepTitle, annotations, setAnnotations,
-    handleNext, handleBack, jobs, rawFileName, useFilepath=false, ...props
-  }) => {
+    handleNext, handleBack, jobs, rawFileName, useFilepath = false, ...props
+}) => {
 
     const fileArg = (useFilepath ? "filepath" : "filename");
     // TODO: Probably need a flag or different endpoint to fetch the entire file data, instead of limited to first 100 rows.
@@ -79,55 +81,55 @@ const Sheet = withStyles(({ spacing }) => ({
                 axios.get(`/api/dojo/indicators/${datasetInfo.id}/annotations`),
                 axios.get(`/api/dojo/indicators/${datasetInfo.id}/data`),
             ])
-        .then(([annotations, data]) => {
-            const metadata = annotations?.data?.metadata;
-            console.log(data);
+            .then(([annotations, data]) => {
+                const metadata = annotations?.data?.metadata;
+                console.log(data);
 
-            if (metadata?.image) {
-                setImage(metadata.image);
-            }
+                if (metadata?.image) {
+                    setImage(metadata.image);
+                }
 
-            // TODO: Remove this, set image ahead of time when appropriate
-            // DEBUG
-            setImage("https://placekitten.com/200/300");
-            setData(data.data);
-        })
-        .catch((e) => {
-            // setPromptMessage('Error loading annotation data.');
-            console.error('Error fetching geoclassify or raw preview:', e);
-        })
-        .finally(() => { setLoading(false); });
+                // TODO: Remove this, set image ahead of time when appropriate
+                // DEBUG
+                setImage(tableImg);
+                setData(data.data);
+            })
+            .catch((e) => {
+                // setPromptMessage('Error loading annotation data.');
+                console.error('Error fetching geoclassify or raw preview:', e);
+            })
+            .finally(() => { setLoading(false); });
     }, [datasetInfo]);
 
     return (
         <Box flex={1}>
             <Box flex={1}>
-                <Box display={"flex"} flex={1} flexDirection={"row"} gridGap={"2em"}>
+                <Box display="flex" flex={1} flexDirection="row" gridGap="2em">
                     {(
-                        loading 
-                        ? <LoadingOverlay text={"Fetching data"}/>
-                        : 
-                        <>
-                            {(
-                                image 
-                                ? <div flex={1} style={{textAlign: 'center'}}>
-                                    <img src={image} />
-                                </div>
-                                : null
-                            )}
-                            <HotTable
-                                flex={3}
-                                data={data}
-                                rowHeaders={true}
-                                contextMenu={true}
-                                mergeCells={true}
-                                width="45%"
-                                stretchH="all"
-                                allowInsertColumn
-                                allowRemoveColumn
-                                licenseKey="non-commercial-and-evaluation" // for non-commercial use only
-                            />
-                        </>
+                        loading
+                            ? <LoadingOverlay text={"Fetching data"} />
+                            :
+                            <>
+                                {(
+                                    image
+                                        ? <div flex={1} style={{ textAlign: 'center' }}>
+                                            <img src={image} />
+                                        </div>
+                                        : null
+                                )}
+                                <HotTable
+                                    flex={3}
+                                    data={data}
+                                    rowHeaders={true}
+                                    contextMenu={true}
+                                    mergeCells={true}
+                                    width="45%"
+                                    stretchH="all"
+                                    allowInsertColumn
+                                    allowRemoveColumn
+                                    licenseKey="non-commercial-and-evaluation" // for non-commercial use only
+                                />
+                            </>
                     )}
                 </Box >
             </Box>
@@ -139,6 +141,6 @@ const Sheet = withStyles(({ spacing }) => ({
             </Container>
         </Box>
     );
-  });
+});
 
 export default Sheet;
