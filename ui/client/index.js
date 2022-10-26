f8e23732885ecf4dd82a0b2719ed0cb8725a467b
Adam Gilgoff <adam.gilg@gmail.com>
2022-10-26 14:04:32 -0400

    Removes model runs, provisioning, and the landing page

--- ui/client/index.js
+++ ui/client/index.js
@@ -18,15 +18,8 @@ import ThemeContextProvider from './components/ThemeContextProvider';
 // pages
 import Admin from './admin';
 import DatasetSummary from './dataset_summary';
-import LandingPage from './landingpage';
-import Provision from './provision';
-import Provisioning from './provisioning';
-import RunLogs from './runlogs';
-import RunSummary from './components/RunSummary';
 import theme from './theme';
 import ViewDatasets from './components/ViewDatasets';
-import ViewModels from './components/ViewModels';
-import ViewRuns from './components/ViewRuns';
 // import DatasetRegistration from './datasets/Register';
 import DatasetRegistrationStepper from './datasets/RegistrationStepper';
 import DatasetPreview from './datasets/Preview';
@@ -37,19 +30,12 @@ export default function Main() {
     <Router>
       <NavBar />
       <Switch>
-        <Route component={LandingPage} exact path="/non-askem" />
-        <Route component={ViewModels} exact path="/models" />
         <Route component={ViewDatasets} exact path="/" />
         <Route component={DatasetAnnotate} exact path="/datasets/annotate" />
         <Route component={DatasetPreview} exact path="/datasets/preview" />
         <Route component={DatasetRegistrationStepper} path="/datasets/:flowslug/:step?/:datasetId?" />
-        <Route component={Provision} exact path="/provision/:modelId" />
-        <Route component={Provisioning} exact path="/provisioning/:modelId" />
         <Route component={DatasetSummary} exact path="/dataset_summary" />
         <Route component={Admin} exact path="/admin" />
-        <Route component={ViewRuns} exact path="/runs" />
-        <Route component={RunSummary} exact path="/runs/:runid" />
-        <Route component={RunLogs} exact path="/runlogs/:runid" />
         <Route path="/*" render={() => <h2>404 Not Found</h2>} />
       </Switch>
     </Router>
