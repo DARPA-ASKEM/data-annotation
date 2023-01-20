e347997a01ac4540724f59fcec4b9391bb46c0f9
Brandon Rose <rose.brandon.m@gmail.com>
2023-01-20 15:12:15 -0600

    updated to use minio

--- api/src/utils.py
+++ api/src/utils.py
@@ -18,8 +18,8 @@ from src.settings import settings
 s3 = boto3.resource(
     "s3",
     endpoint_url=os.getenv("STORAGE_HOST"),
-    aws_access_key_id=os.getenv("S3_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("S3_SECRET_ACCESS_KEY"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     aws_session_token=None,
     config=boto3.session.Config(signature_version="s3v4"),
     verify=False,
